d7f202779a0c37cda48ea487cc4a93325ca14d08
samyak257 <sammuhere@gmail.com>
2025-05-18 15:05:49 -0400

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+# import csv
+
+# with open('matches.csv', 'r') as file:
+
+#     reader = csv.DictReader(file)
+
+#     with open ('new_matches.csv', 'w') as new_file:
+#         csv_writer = csv.writer(new_file, delimiter="\t")
+        
+        
+#         for line in reader:
+#             csv_writer.writerow(line)
+
+import pandas as pd
+
+
+
+
+
+
