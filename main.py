e9c0ae941bb7522b7f95a1dc6da42727f26bed8c
samyak257 <sammuhere@gmail.com>
2025-05-18 17:28:08 -0400

    cleaned data

--- main.py
+++ main.py
@@ -14,11 +14,21 @@
 import pandas as pd
 import streamlit as st
 
-def load_data():
+
+def load_and_clean_data():
     df = pd.read_csv('matches.csv')
+    
+    #clean data
+    df = df.drop(columns=["method"], errors="ignore")
+    df["super_over"] = df["super_over"].map({"Y": True, "N": False})
+    df["player_of_match"] = df["player_of_match"].fillna("No player of match")
+    df["umpire1"] = df["umpire1"].fillna("No umpire")
+    df["umpire2"] = df["umpire2"].fillna("No umpire")
+
     return df
 
-df = load_data()
+
+df = load_and_clean_data()
 
 st.title("IPL Match Visualizer")
 st.write("Here is a sample of the dataset:")
